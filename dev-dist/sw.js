7d72fb95ce856d63bbd2e785dde9b7815acae79e
Josewesley2020 <josewesley2020@users.noreply.github.com>
2025-12-12 01:37:26 -0300

    feat: add down payment percentage functionality in PriceCalculator and Settings components

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.q5me7o99o74"
+    "revision": "0.55qdippab4o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
